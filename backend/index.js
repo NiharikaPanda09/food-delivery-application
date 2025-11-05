bba3d577cc30e0f3441b9e868f7a6b5f89cb4281
NiharikaPanda09 <132553665+NiharikaPanda09@users.noreply.github.com>
2025-11-06 01:07:39 +0530

    Add CORS support to the Express app

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 5000
-
+const cors = require('cors');
 const mongoDB = require('./db')
 mongoDB()
-
+app.use(cors());
 app.use((req, res, next) => {
   const allowedOrigins = [  
     "*"
